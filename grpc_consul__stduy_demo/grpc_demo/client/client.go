f4d5a2c536eb21b0681e6968b30027a9ed3af7ec
HUzhiqing <nuszhiqinghu@gmail.com>
2023-04-23 22:49:47 +0800

    amend some logic error

--- grpc_consul__stduy_demo/grpc_demo/client/client.go
+++ grpc_consul__stduy_demo/grpc_demo/client/client.go
@@ -17,11 +17,16 @@ const (
 )
 
 func main() {
+	//连接到server端，此处禁用安全连接，没有加密和验证
+	//conn是连接对象
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+	//所有的连接都要进行关闭
 	defer conn.Close()
+
+	//建立连接 创立了一个具有连接的c客户端 建立连接之后就可以直接调用服务端的代码
 	c := pb.NewGreeterClient(conn)
 
 	name := defaultName
@@ -30,6 +35,8 @@ func main() {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
+	//执行rpc调用
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
--- grpc_consul__stduy_demo/grpc_demo/server/server.go
+++ grpc_consul__stduy_demo/grpc_demo/server/server.go
@@ -22,14 +22,20 @@ func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloR
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port) // 监听 TCP 端口
+	// 开启端口
+	lis, err := net.Listen("tcp", port)
+	// 如果监听失败，则退出程序并打印错误信息
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err) // 如果监听失败，则退出程序并打印错误信息
+		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()                  // 创建一个 gRPC 服务实例
-	pb.RegisterGreeterServer(s, &server{}) // 注册服务，把实现服务的 server 结构体绑定到 gRPC 服务实例中
+	// 创建一个 gRPC 服务实例
+	s := grpc.NewServer()
+	// 注册服务，把实现服务的 server 结构体绑定到 gRPC 服务实例中
+	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("start gRPC server on port %s\n", port)
-	if err := s.Serve(lis); err != nil { // 开始监听
-		log.Fatalf("failed to serve: %v", err) // 开始监听，如果监听失败，则退出程序并打印错误信息
+	// .Server启动服务，监听lis
+	// 如果监听失败，则退出程序并打印错误信息
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
